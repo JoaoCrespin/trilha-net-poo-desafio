3fbb423215bcdf5887da0305d186023492077cc7
João Crespin <joaorato58@gmail.com>
2024-11-16 00:22:31 -0300

    Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Nokia nokia = new Nokia("12999999999", "Tijolinho", "111111", 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Zuma");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Iphone iphone = new Iphone("12998888888","16","222222",64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Subway Surfers");
